fd46171834f6a97cb13f188bb714cac29f546fb5
Jefferson Ayovi <105981230+jeffayovi@users.noreply.github.com>
2026-01-06 11:06:07 -0500

    Comparación simple: Introducción al if con dos valores.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+/**
+ * Escriba una aplicación que pida al usuario que escriba dos enteros, que obtenga
+ * los números del usuario y muestre el número más grande, seguido de las palabras “es más grande”. Si los números son
+ * iguales, imprima el mensaje “Estos números son iguales”
+ *
+ * */
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner entrada =new Scanner(System.in);
+
+        //Pedir y leer dos numeros
+        System.out.println(" Escribe el primer numero: ");
+        int numero1 = entrada.nextInt();
+
+        System.out.println(" Escribe el segundo numero: ");
+        int numero2 = entrada.nextInt();
+
+        //Mostrar mensajes
+        if (numero1 > numero2)
+            System.out.println(numero1 + " es mas grande "+ numero2);
+        if (numero2 > numero1)
+            System.out.println(numero2 + " es mas grande "+ numero1);
+        if (numero2 == numero1)
+            System.out.println(" Estos números son iguales ");
+
+        entrada.close();
+
+
     }
 }
